d680879686928e5be939959bf41b1664a18e8d03
Brad Buran <buran@ohsu.edu>
2024-12-26 15:15:41 -0800

    Collapse inputs into drop-down list

--- cftscal/plugins/microphone/settings.py
+++ cftscal/plugins/microphone/settings.py
@@ -1,6 +1,8 @@
+import json
+
 from atom.api import List, Typed
 
-from psi import get_config
+from psi import get_config, get_config_folder
 
 from ..settings import CalibrationSettings, MicrophoneSettings, PistonphoneSettings
 
@@ -9,6 +11,7 @@ from cftscal import CAL_ROOT
 
 class MicrophoneCalibrationSettings(CalibrationSettings):
 
+    selected_input = Typed(MicrophoneSettings, ())
     microphones = List(Typed(MicrophoneSettings, ()))
     pistonphone = Typed(PistonphoneSettings, ())
 
@@ -16,11 +19,29 @@ class MicrophoneCalibrationSettings(CalibrationSettings):
         self.microphones = [MicrophoneSettings(input_label=k, input_name=v) \
                             for k, v in inputs.items()]
         self.pistonphone = PistonphoneSettings()
+        self.selected_input = self.microphones[0]
+        self.load_config()
 
     def save_config(self):
         for m in self.microphones:
             m.save_config()
         self.pistonphone.save_config()
+        file = get_config_folder() / 'cfts' / 'calibration' / \
+            'microphone_calibration.json'
+        config = {'selected_input': self.selected_input.input_name}
+        file.write_text(json.dumps(config, indent=2))
+
+    def load_config(self):
+        file = get_config_folder() / 'cfts' / 'calibration' / \
+            'microphone_calibration.json'
+        if not file.exists():
+            return
+        config = json.loads(file.read_text())
+        selected_input = config.pop('selected_input')
+        for microphone in self.microphones:
+            if microphone.input_name == selected_input:
+                self.selected_input = microphone
+                break
 
     def run_mic_cal(self, microphone):
         filename = f'{{date_time}}_{microphone.connected_device}_{self.pistonphone.name}'
