81093817a62f5647ec49eada22d88152d683f093
Brad Buran <buran@ohsu.edu>
2025-02-12 15:16:43 -0800

    Rename environment variable to CFTSCAL_ROOT

--- cftscal/__init__.py
+++ cftscal/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 
 DEFAULT_CAL_ROOT = os.path.expanduser('~/Documents/cftscal')
-CAL_ROOT = Path(os.environ.get('CAL_ROOT', DEFAULT_CAL_ROOT))
+CAL_ROOT = Path(os.environ.get('CFTSCAL_ROOT', DEFAULT_CAL_ROOT))
