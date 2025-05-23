3c508279f112540f5a5efe57aa9da147b2a988bd
Brad Buran <buran@ohsu.edu>
2025-05-23 07:10:22 -0700

    Log base folder of calibration to help with troubleshooting

--- cftscal/__init__.py
+++ cftscal/__init__.py
@@ -1,6 +1,10 @@
+import logging
+log = logging.getLogger(__name__)
+
 import os
 from pathlib import Path
 
 
 DEFAULT_CAL_ROOT = os.path.expanduser('~/Documents/cftscal')
 CAL_ROOT = Path(os.environ.get('CFTSCAL_ROOT', DEFAULT_CAL_ROOT))
+log.info('Base folder for calibrations is %s', CAL_ROOT)
