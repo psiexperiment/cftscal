2ef943e502fb917d47a76478aff0dcef24ba5ffd
Brad Buran <buran@ohsu.edu>
2025-06-12 14:44:31 -0700

    Add additional functions to API

--- cftscal/api.py
+++ cftscal/api.py
@@ -1,10 +1,17 @@
 from .util import (
+    list_generic_microphone_connections,
     list_input_amplifier_connections,
+    list_measurement_microphone_connections,
     list_microphone_connections,
     list_speaker_connections,
     list_starship_connections
 )
+
 from .objects import (
-    generic_microphone_manager, inear_manager, input_amplifier_manager,
-    measurement_microphone_manager, speaker_manager, starship_manager
+    generic_microphone_manager,
+    inear_manager,
+    input_amplifier_manager,
+    measurement_microphone_manager,
+    speaker_manager,
+    starship_manager
 )
