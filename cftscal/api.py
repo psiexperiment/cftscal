f1af5bfe0adb5bb40e7d8e4c50abbdf5e18459b2
Brad Buran <buran@ohsu.edu>
2026-04-08 15:48:44 -0700

    Misc tweaks

--- cftscal/api.py
+++ cftscal/api.py
@@ -2,7 +2,6 @@ from .util import (
     list_generic_microphone_connections,
     list_input_amplifier_connections,
     list_measurement_microphone_connections,
-    list_microphone_connections,
     list_speaker_connections,
     list_starship_connections
 )
